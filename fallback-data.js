4c351ef46a2c75d865c512aca44f99527ea109f9
ag2328 <andrewggaitan@gmail.com>
2025-05-03 23:52:50 -0700

    Delete fallback-data.js

--- fallback-data.js
+++
@@ -1,8 +0,0 @@
-// Fallback data in case Google Sheets is unavailable
-const FALLBACK_DATA = [
-  { team: "Red Wings", w: 5, l: 2, t: 1, pts: 11, gf: 25, ga: 18 },
-  { team: "Maple Leafs", w: 4, l: 3, t: 1, pts: 9, gf: 22, ga: 20 },
-  { team: "Bruins", w: 3, l: 3, t: 2, pts: 8, gf: 19, ga: 19 },
-  { team: "Canadiens", w: 2, l: 5, t: 1, pts: 5, gf: 15, ga: 24 },
-  { team: "Lightning", w: 1, l: 6, t: 1, pts: 3, gf: 12, ga: 22 }
-];
